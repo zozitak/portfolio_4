379dd1a270403b397cd054a8256a374d8d79b16a
Takács Zoltán Gábor <zozitak@gmail.com>
2024-05-09 14:03:06 +0200

    main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# main.py
+
+import sys
+
+from cqplugin.export_step import export_step_ap214
+
+def main() -> int:
+    # create cq model
+    # mesh it 
+    # simulation
+    # analysis
+    # saveing
+    return 0 
+
+if __name__ == '__main__':
+    sys.exit(main())
