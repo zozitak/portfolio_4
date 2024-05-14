5952d4cc1d1670115d00d0a3702ed365d280780d
Takács Zoltán Gábor <zozitak@gmail.com>
2024-05-14 09:34:52 +0200

    Scaled variables

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 # main.py
 
+import cadquery as cq
 import sys
 
 from cqplugin.export_step import export_step_ap214
 
 def main() -> int:
+
+    # Scaled variables
+    L = 1; W = 0.2
+    mu = 1
+    rho = 1
+    delta = W/L
+    gamma = 0.4*delta**2
+    beta = 1.25
+    lambda_ = beta
+    g = gamma
+
     # create cq model
+
     # mesh it 
     # simulation
     # analysis
