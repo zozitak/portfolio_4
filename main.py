102515b39f1a6b4b727d07fba1fbd09ac52c1326
Takács Zoltán Gábor <zozitak@gmail.com>
2024-05-24 18:17:41 +0200

    cleanup

--- main.py
+++ main.py
@@ -9,38 +9,31 @@ import sys
 
 from cqplugin.export_step import export_step_ap214
 
-def main() -> int:
+# Scaled variables
+L = 1.0; W = 0.2
+mu = 1
+rho = 1
+delta = W/L
+gamma = 0.4*delta**2
+beta = 1.25
+lambda_ = beta
+g = gamma
 
-    # Scaled variables
-    L = 1.0; W = 0.2
-    mu = 1
-    rho = 1
-    delta = W/L
-    gamma = 0.4*delta**2
-    beta = 1.25
-    lambda_ = beta
-    g = gamma
+# create cadquery model
+cad_model = cadquery.Workplane("xy").box(L, W, W)
+cad_exchange = cad_model.toOCC()
 
-    # create cadquery model
-    cad_model = cadquery.Workplane("xy").box(L, W, W)
-    cad_exchange = cad_model.toOCC()
+# mesh it 
+gmsh.initialize()
+gmsh.model.occ.importShapesNativePointer(cad_exchange._address())
+gmsh.model.occ.synchronize()
 
-    # mesh it 
-    gmsh.initialize()
-    gmsh.model.occ.importShapesNativePointer(cad_exchange._address())
-    gmsh.model.occ.synchronize()
-    
 
-    # simulation
-    gmsh_model_rank = 0
-    mesh_comm = MPI.COMM_WORLD
-    domain, cell_markers, facet_markers = gmshio.model_to_mesh(gmsh.model, mesh_comm, gmsh_model_rank, gdim=gdim)
-    gmsh.finalize()
+# simulation
+gmsh_model_rank = 0
+mesh_comm = MPI.COMM_WORLD
+domain, cell_markers, facet_markers = gmshio.model_to_mesh(gmsh.model, mesh_comm, gmsh_model_rank, gdim=gdim)
+gmsh.finalize()
 
-    # analysis
-    # saving
-
-    return 0 
-
-if __name__ == '__main__':
-    sys.exit(main())
+# analysis
+# saving
