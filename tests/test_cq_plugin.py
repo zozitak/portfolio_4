f0aaf901f7080b060778a7b8184467855cb1a7ce
Takács Zoltán Gábor <zozitak@gmail.com>
2024-06-03 19:29:20 +0200

    test: step export

--- tests/test_cq_plugin.py
+++ tests/test_cq_plugin.py
@@ -4,14 +4,8 @@ import unittest
 import sys
 import os
 
-def makeCube(size, xycentered=True):
-    if xycentered:
-        return Workplane().rect(size, size).extrude(size).val()
-    else:
-        return Solid.makeBox(size, size, size)
-
-def makeUnitCube(centered=True):
-    return makeCube(1.0, centered)
+def makeUnitCube():
+    return Workplane().rect(1.0, 1.0).extrude(1.0).val()
 
 def readFileAsString(fileName):
     f = open(fileName, "r")
@@ -19,5 +13,22 @@ def readFileAsString(fileName):
     f.close()
     return s
 
+class TestSTEPexport(unittest.TestCase):
+    def test_step_export(self):
+        cwd = os.getcwd()
+        tmpfile = "temp.step"
+        #create something
+        modell = makeUnitCube()
+        assembly = Assembly()
+        assembly.add(modell,loc=Location((0, 0, 0), (1, 0, 0), 180),name="Test text.")
+        #save step to tmp file
+        assembly.save(os.path.join(cwd,tmpfile))
+        #read tmp file and comper result
+        FileAsString = readFileAsString(os.path.join(cwd,tmpfile))
+        #remove tmp file
+        os.remove(os.path.join(cwd,tmpfile))
+        self.assertRegex(FileAsString,"Test text.")
 
-
+class TestSTEPap214export(unittest.TestCase):
+    def test_step_ap214_export(self):
+        raise NotImplemented
